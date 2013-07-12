b60b2255ad1a2581379506545cf7ad527ee36ad3
eliorivero <eliorivero@gmail.com>
2013-07-12 17:18:46 -0300

    Update ilc-logbox.php

--- ilc-logbox.php
+++ ilc-logbox.php
@@ -18,7 +18,6 @@ class TR_LogBox {
 	}
 	
 	function shortcode($atts) {
-	    global $user_login;
 		extract( shortcode_atts( array(
 			'login_redirect' => 'admin', // admin | same_page
 			'logout_redirect' => 'same_page' // same_page
@@ -47,22 +46,22 @@ class TR_LogBox {
 				break;
 		}
 		
-	    if ( !is_user_logged_in() ){
-	        $html = '
-	        <form class="logbox" action="' . wp_login_url() . '" method="post">
-	            <input type="text" placeholder="' . __('Username', 'ilc') . '" name="log" id="log" value="" size="20" />
-	            <input type="password" placeholder="' . __('Password', 'ilc') . '" name="pwd" id="pwd" size="20" />
-	            <input type="submit" name="submit" value="' . __('Log In', 'ilc') . '" class="button" />
-	            <label for="rememberme"><input name="rememberme" id="rememberme" type="checkbox" checked="checked" value="forever" /> ' . __('Remember Me', 'ilc') . '</label>
-	            <input type="hidden" name="redirect_to" value="' . $login_redirect . '" />
+		if ( !is_user_logged_in() ){
+		$html = '
+		<form class="logbox" action="' . wp_login_url() . '" method="post">
+		    <input type="text" placeholder="' . __('Username', 'ilc') . '" name="log" id="log" value="" size="20" />
+		    <input type="password" placeholder="' . __('Password', 'ilc') . '" name="pwd" id="pwd" size="20" />
+		    <input type="submit" name="submit" value="' . __('Log In', 'ilc') . '" class="button" />
+		    <label for="rememberme"><input name="rememberme" id="rememberme" type="checkbox" checked="checked" value="forever" /> ' . __('Remember Me', 'ilc') . '</label>
+		    <input type="hidden" name="redirect_to" value="' . $login_redirect . '" />
 			</form>
 			<p class="recover-pass">
 				<a href="' . site_url('/wp-login.php?action=lostpassword') . '">' . __('Lost your password?', 'ilc') . '</a>
 			</p>';
-	    } else {
-	        $html = '<p class="is_logged_in">' . __('You are already logged in.', 'ilc') . ' <a href="' . wp_logout_url($logout_redirect) . '">' . __('Log Out?', 'ilc') . '</a></p>';
-	    }
-	    return $html;
+		} else {
+		$html = '<p class="is_logged_in">' . __('You are already logged in.', 'ilc') . ' <a href="' . wp_logout_url($logout_redirect) . '">' . __('Log Out?', 'ilc') . '</a></p>';
+		}
+		return $html;
 	}
 
 	function localization() {
